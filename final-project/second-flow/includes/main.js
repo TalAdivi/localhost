b0a2c8dde6423542349d1ee8920e9c918c1e409d
TalAdivi <taladivi10@gmail.com>
2019-06-17 19:42:48 +0300

    update

--- final-project/second-flow/includes/main.js
+++ final-project/second-flow/includes/main.js
@@ -1,71 +1,63 @@
-$(document).ready(function(){
-  
-  //to change only the logo size because the other style are the same as mobile
-  $(window).on('resize',function(size){
-    var windowWidth = $(window).width();
-    if(windowWidth <= 768 && windowWidth >= 481){
-      $('#logo').attr({style: 'background-image: url(./images/largeLogo.png); width:600px; height:500px;'});
-      $('.move-up').attr({style:'margin-bottom: -180px;'})
-    }
-  })
+$(document).ready(function () {
+
 });
 
-    // round progress bar logic
-    $.fn.loading = function () {
-      var DEFAULTS = {
-        backgroundColor: '#b3cef6',
-        progressColor: '#4b86db',
-        percent: 75,
-        duration: 2000
-      };	
-      
-      $(this).each(function () {
-        var $target  = $(this);
-  
-        var opts = {
-        backgroundColor: $target.data('color') ? $target.data('color').split(',')[0] : DEFAULTS.backgroundColor,
-        progressColor: $target.data('color') ? $target.data('color').split(',')[1] : DEFAULTS.progressColor,
-        percent: $target.data('percent') ? $target.data('percent') : DEFAULTS.percent,
-        duration: $target.data('duration') ? $target.data('duration') : DEFAULTS.duration
-        };
-        // console.log(opts);
-    
-        $target.append('<div class="background"></div><div class="rotate"></div><div class="left"></div><div class="right"></div><div class=""><span>' + opts.percent + '%</span></div>');
-    
-        $target.find('.background').css('background-color', opts.backgroundColor);
-        $target.find('.left').css('background-color', opts.backgroundColor);
-        $target.find('.rotate').css('background-color', opts.progressColor);
-        $target.find('.right').css('background-color', opts.progressColor);
+// round progress bar logic
+$.fn.loading = function () {
+  var DEFAULTS = {
+    backgroundColor: '#b3cef6',
+    progressColor: '#4b86db',
+    percent: 75,
+    duration: 2000
+  };
 
-        $('.show-percent').append(opts.percent + '%');
-    
-        var $rotate = $target.find('.rotate');
-        setTimeout(function () {	
-          $rotate.css({
-            'transition': 'transform ' + opts.duration + 'ms linear',
-            'transform': 'rotate(' + opts.percent * 3.6 + 'deg)'
-          });
-        },1);		
-  
-        if (opts.percent > 50) {
-          var animationRight = 'toggle ' + (opts.duration / opts.percent * 50) + 'ms step-end';
-          var animationLeft = 'toggle ' + (opts.duration / opts.percent * 50) + 'ms step-start';  
-          $target.find('.right').css({
-            animation: animationRight,
-            opacity: 1
-          });
-          $target.find('.left').css({
-            animation: animationLeft,
-            opacity: 0
-          });
-        } 
+  $(this).each(function () {
+    var $target = $(this);
+
+    var opts = {
+      backgroundColor: $target.data('color') ? $target.data('color').split(',')[0] : DEFAULTS.backgroundColor,
+      progressColor: $target.data('color') ? $target.data('color').split(',')[1] : DEFAULTS.progressColor,
+      percent: $target.data('percent') ? $target.data('percent') : DEFAULTS.percent,
+      duration: $target.data('duration') ? $target.data('duration') : DEFAULTS.duration
+    };
+    // console.log(opts);
+
+    $target.append('<div class="background"></div><div class="rotate"></div><div class="left"></div><div class="right"></div><div class=""><span>' + opts.percent + '%</span></div>');
+
+    $target.find('.background').css('background-color', opts.backgroundColor);
+    $target.find('.left').css('background-color', opts.backgroundColor);
+    $target.find('.rotate').css('background-color', opts.progressColor);
+    $target.find('.right').css('background-color', opts.progressColor);
+
+    $('.show-percent').append(opts.percent + '%');
+
+    var $rotate = $target.find('.rotate');
+    setTimeout(function () {
+      $rotate.css({
+        'transition': 'transform ' + opts.duration + 'ms linear',
+        'transform': 'rotate(' + opts.percent * 3.6 + 'deg)'
+      });
+    }, 1);
+
+    if (opts.percent > 50) {
+      var animationRight = 'toggle ' + (opts.duration / opts.percent * 50) + 'ms step-end';
+      var animationLeft = 'toggle ' + (opts.duration / opts.percent * 50) + 'ms step-start';
+      $target.find('.right').css({
+        animation: animationRight,
+        opacity: 1
+      });
+      $target.find('.left').css({
+        animation: animationLeft,
+        opacity: 0
       });
     }
- 
-  
+  });
+}
+
+
+$(".progress-bar").loading();
+$('input').on('click', function () {
   $(".progress-bar").loading();
-      $('input').on('click', function () {
-         $(".progress-bar").loading();
-      });
-  
+});
+
 
