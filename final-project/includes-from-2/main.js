8e26b6731099ea6ccb0969fc3fb897be1231e8b5
TalAdivi <taladivi10@gmail.com>
2019-06-17 23:23:21 +0300

    marged flow 1 and flow 2

--- final-project/includes-from-2/main.js
+++ final-project/includes-from-2/main.js
@@ -1,54 +1,30 @@
+$(document).ready(function () {
+  clicked = false;
+  $( "button" ).on( "click", function( event ) {
+    var target = $( event.target ),
+      progressbar = $( ".progress-bar" ),
+      progressbarValue = progressbar.find( ".progress-bar" );
+      
 
-clicked = false;
-$( "button" ).on( "click", function( event ) {
-  var target = $( event.target ),
-    progressbar = $( ".progress-bar" ),
-    progressbarValue = progressbar.find( ".progress-bar" );
-    
+     if ( target.is( "#colorButton" ) ) {
+         console.log('here');
+      
+         progressbar.css({"background-color": '#' + Math.floor( Math.random() * 16777215 ).toString( 16 )});
 
-   if ( target.is( "#colorButton" ) ) {
-       console.log('here');
-    
-       progressbar.css({"background-color": '#' + Math.floor( Math.random() * 16777215 ).toString( 16 )});
-
-  } else if ( target.is( "#falseButton" ) ) {
-      if(!clicked){
-          progressbar.addClass("progress-bar-striped");
-          progressbar.addClass("progress-bar-animated");
-        clicked = true;
-      }
-      else{
-          console.log("in else");
-        progressbar.removeClass("progress-bar-striped");
-        progressbar.removeClass("progress-bar-animated");
-        clicked = false;
-      }
-  }
+    } else if ( target.is( "#falseButton" ) ) {
+        if(!clicked){
+            progressbar.addClass("progress-bar-striped");
+            progressbar.addClass("progress-bar-animated");
+          clicked = true;
+        }
+        else{
+            console.log("in else");
+          progressbar.removeClass("progress-bar-striped");
+          progressbar.removeClass("progress-bar-animated");
+          clicked = false;
+        }
+    }
 });
-
-$("button").on("click",function(curr){
-  $("button").removeClass("active");
-  $(this).toggleClass("active");
-})
-
-
-$('.addIcon').on("click",function(){
-  var table_row = $(
-      '<tr>' +
-      '<td>' +
-      '<input type="text" class="form-control" placeholder="Product name">' +
-      '</td>' +
-      '<td>' +
-      '<input type="number" class="form-control" placeholder="0">' +
-      '</td>' +
-      '</tr>'
-  )
-  $('.mainTableBody').append(table_row);
-})
-
-
-
-
 
 // round progress bar logic
 $.fn.loading = function () {
